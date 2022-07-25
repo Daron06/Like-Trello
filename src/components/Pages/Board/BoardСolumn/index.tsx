a51c083cd0453496fe0c57a74239943f006ee49d
Daron <ulaire06@gmail.com>
2022-07-25 17:58:57 +0300

    Перехожу на библиотеку

--- src/components/Pages/Board/BoardСolumn/index.tsx
+++ src/components/Pages/Board/BoardСolumn/index.tsx
@@ -3,61 +3,12 @@ import { BoardButton } from '../BoardButton';
 import { BoardCard } from '../BoardCard';
 import styles from './BoardСolumn.module.scss';
 
-export const BoardСolumn: React.FC<{ title: string; id: number }> = ({ title, id }) => {
-  const [positionEl, setPositionEl] = React.useState({
-    top: 0,
-    left: 0,
-    abs: false,
-    heightEl: 0,
-    zI: 1000,
-  });
-  const cardRef = React.useRef<any>(null);
-
-  function onMouseEvent(event: any) {
-    let shiftX = event.clientX - cardRef.current.getBoundingClientRect().left;
-    let shiftY = event.clientY - cardRef.current.getBoundingClientRect().top;
-
-    function moveAt(pageX: number, pageY: number) {
-      if (cardRef.current) {
-        setPositionEl({
-          left: pageX - shiftX,
-          top: pageY - shiftY,
-          abs: true,
-          heightEl: cardRef.current?.clientHeight,
-          zI: 2000,
-        });
-      }
-    }
-
-    function onMouseMove(e: any) {
-      moveAt(e.pageX, e.pageY);
-      console.log(e);
-    }
-
-    // передвигаем  при событии mousemove
-    document.addEventListener('mousemove', onMouseMove);
-
-    //  удалить ненужные обработчики
-    cardRef.current.onmouseup = function () {
-      document.removeEventListener('mousemove', onMouseMove);
-      cardRef.current.onmouseup = null;
-      setPositionEl({
-        left: 0,
-        top: 0,
-        abs: false,
-        heightEl: 0,
-        zI: 1000,
-      });
-    };
-  }
-
-  React.useEffect(() => {
-    const mouseEvent = cardRef.current;
-    mouseEvent?.addEventListener('mousedown', onMouseEvent);
-
-    return () => mouseEvent?.removeEventListener('mousedown', onMouseEvent);
-  }, []);
-
+export const BoardСolumn: React.FC<{
+  title: string;
+  id: string;
+  positionEl: any;
+  cardRef: any;
+}> = ({ title, positionEl, id }) => {
   return (
     <>
       {positionEl.abs && (
@@ -70,7 +21,6 @@ export const BoardСolumn: React.FC<{ title: string; id: number }> = ({ title, i
         ></div>
       )}
       <div
-        ref={cardRef}
         style={{
           top: positionEl.top,
           left: positionEl.left,
@@ -81,8 +31,8 @@ export const BoardСolumn: React.FC<{ title: string; id: number }> = ({ title, i
         className={styles.root}
       >
         <div>
-          <div className={styles.header}>
-            <p>{<textarea value={title} />}</p>
+          <div className={styles.header} id={String(id)}>
+            <p>{<textarea placeholder={title} />}</p>
           </div>
           <div className={styles.content}>
             <BoardCard />
--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -4,26 +4,108 @@ import styles from './Boards.module.scss';
 import { BoardСolumn } from '../../components/Pages/Board/BoardСolumn';
 import { BoardButton } from '../../components/Pages/Board/BoardButton';
 
-const boardColumn = [
-  { title: 'Некий тайтл', id: 1 },
-  { title: 'Некий тайтл 2', id: 2 },
-];
-
 export const Boards: React.FC = () => {
-  const boardRef = React.useRef<HTMLDivElement>(null);
-
-  // React.useEffect(() => {
-  //   boardRef?.current?.addEventListener('mouseenter', (event) => {
-  //     console.log('board', event, event.currentTarget, event.relatedTarget);
-  //   });
-  // });
-
   let params = useParams();
-  console.log(params);
+  const [positionEl, setPositionEl] = React.useState([
+    {
+      title: 'Некий тайтл',
+      id: '1/column/d&d',
+      pos: {
+        top: 0,
+        left: 0,
+        abs: false,
+        heightEl: 0,
+        zI: 1000,
+      },
+    },
+    {
+      title: 'Некий тайтл 2',
+      id: '2/column/d&d',
+      pos: {
+        top: 0,
+        left: 0,
+        abs: false,
+        heightEl: 0,
+        zI: 1000,
+      },
+    },
+  ]);
+
+  const cardRef = React.useRef<any>(null);
+
+  function onMouseEvent(event: any) {
+    if (!event?.target?.id?.includes('/column/d&d')) {
+      return;
+    }
+
+    let shiftX = event.clientX - event.target.getBoundingClientRect().left;
+    let shiftY = event.clientY - event.target.getBoundingClientRect().top;
+
+    function moveAt(pageX: number, pageY: number) {
+      if (event.target) {
+        setPositionEl((prev) =>
+          prev.map((el) =>
+            el.id === event.target.id
+              ? {
+                  ...el,
+                  pos: {
+                    left: pageX - shiftX,
+                    top: pageY - shiftY,
+                    abs: true,
+                    heightEl: event.target?.clientHeight,
+                    zI: 2000,
+                  },
+                }
+              : el
+          )
+        );
+      }
+    }
+
+    function onMouseMove(e: any) {
+      moveAt(e.pageX, e.pageY);
+    }
+    function onMouseOut(e: any) {
+      if (e.target?.id?.includes('/column/d&d')) {
+        console.log(event.target.id, e.target.id, e.relatedTarget.id);
+      }
+    }
+
+    document.addEventListener('mouseout', onMouseOut);
+
+    // передвигаем  при событии mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    //  удалить ненужные обработчики
+    document.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseout', onMouseOut);
+      event.target.onmouseup = null;
+      setPositionEl((prev) =>
+        prev.map((el) => ({
+          ...el,
+          pos: {
+            top: 0,
+            left: 0,
+            abs: false,
+            heightEl: 0,
+            zI: 1000,
+          },
+        }))
+      );
+    };
+  }
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', onMouseEvent);
+
+    return () => document.removeEventListener('mousedown', onMouseEvent);
+  }, []);
+
   return (
-    <div ref={boardRef} className={styles.root}>
-      {boardColumn.map((column) => (
-        <BoardСolumn key={column.id} {...column} />
+    <div className={styles.root}>
+      {positionEl.map((column) => (
+        <BoardСolumn cardRef={cardRef} positionEl={column.pos} key={column.id} {...column} />
       ))}
       <BoardButton imgType="board-plus" text="Добавьте ещё одну колонку" />
     </div>
