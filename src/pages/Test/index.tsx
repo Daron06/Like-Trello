2a6528fa48ac5bf14b4cfdff021da4c872a97c75
Daron <ulaire06@gmail.com>
2023-04-13 15:39:02 +0300

    b

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -9,8 +9,9 @@ export const Test = () => {
         <button onClick={() => setOpen((prev) => !prev)}>click</button>
       </header>
       <div className={s.container}>
-        <div className={s.wrapper}>
+        <div className={open && s.wrapper}>
           <div className={open ? `${s.primaryMenu} ${s.primaryMenu__open}` : s.primaryMenu}>
+            <button onClick={() => setOpen((prev) => !prev)}>click</button>
             nav
           </div>
         </div>
