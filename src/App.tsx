1e18005db1c35870b29559a0aeea9361c8b79947
Daron <ulaire06@gmail.com>
2023-04-25 16:01:07 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,77 @@ import { Boards } from './pages/Boards';
 import { Calendar } from './pages/Calendar';
 import { MainLayout } from './layouts/MainLayout';
 import { Test } from './pages/Test';
+import d from 'detect.js';
+const arr = ['89851234567', '88005555777', '84957556983', '848221234567', '84842575018'];
+
+const phoneFormatSettings = {
+  defaultPattern: /(\+7|7|8)(\d{3})(\d{3})(\d{2})(\d{2})/g,
+  defaultFormat: '+7 ($2) $3-$4-$5',
+  '4842': {
+    pattern: /(\+7|7|8)(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/g,
+    format: '+7 ($2-$3) $4-$5-$6',
+  },
+  '4822': {
+    pattern: /(\+7|7|8)(\d{4})(\d{3})(\d{2})(\d{2})/g,
+    format: null,
+  },
+  '800': {
+    pattern: null,
+    format: '8 ($2) $3-$4-$5',
+  },
+};
+
+console.log(d.parse(navigator.userAgent), navigator);
+const { userAgent } = navigator;
+if (userAgent.includes('Edg') || userAgent.includes('YaBrowser')) {
+  console.log(true);
+}
 
 function App() {
+  const maskPhoneNumber = (phone: string) =>
+    phone.replace(
+      /^(?:\+7|8)(\s\(\d{3}\)|\s\(\d{4}\)|\s\(\d{2}-\d{2}\))\s(\d{1,4})\s?(-)?(\d{2})-(\d{2})$/,
+      (match, p1, p2, p3, _, p5) =>
+        (match.startsWith('8') && p1.includes('800') ? '8' : '+7') +
+        p1 +
+        ' ' +
+        '*'.repeat(p2.length) +
+        (p3 ? '-' : '') +
+        '**-' +
+        p5
+    );
+
+  const formatterPhoneNumber = (phone: string, isMasked?: boolean): string => {
+    if (phone.length < 8) {
+      return phone;
+    }
+
+    if (phone.startsWith('+')) {
+      phone = phone.slice(1);
+    }
+
+    const setting =
+      phoneFormatSettings[phone.slice(1, 4) as keyof object] ||
+      phoneFormatSettings[phone.slice(1, 5) as keyof object] ||
+      phoneFormatSettings;
+    const mask = setting['format'] || phoneFormatSettings.defaultFormat;
+    const pattern = setting['pattern'] || phoneFormatSettings.defaultPattern;
+
+    const phoneNumber = phone.replace(pattern, mask);
+
+    if (isMasked) {
+      return maskPhoneNumber(phoneNumber);
+    }
+
+    return phoneNumber;
+  };
+
+  // console.log(formatterPhoneNumber('+79851234567', true));
+  // console.log(formatterPhoneNumber('88005555777'));
+  // console.log(formatterPhoneNumber('+84842575018'));
+
+  console.log();
+
   return (
     <div
       className="App"
