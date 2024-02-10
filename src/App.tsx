108ee6c063c367b6277dea77015ebc9db4163aff
Ibragim <55581144+Daron06@users.noreply.github.com>
2024-02-10 17:18:19 +0000

    тест

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,14 @@ import { Messages } from './pages/Messages';
 import { Boards } from './pages/Boards';
 import { Calendar } from './pages/Calendar';
 import { MainLayout } from './layouts/MainLayout';
-import { Test } from './pages/Test';
-import { detect, detectOS, browserName } from 'detect-browser';
-
-const browser = detect();
-
-console.log(detectOS(navigator.userAgent), browserName(navigator.userAgent));
-
-const checkBrowser = () => {
-  switch (browser && browser.name) {
-    case 'yandexbrowser':
-    case 'safari':
-    case 'edge':
-    case 'edge-chromium':
-      return true;
-    default:
-      return false;
-  }
-};
 
 function App() {
-  console.log(
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|IOS/i.test(
-      detectOS(navigator.userAgent) || ''
-    ),
-    navigator.userAgent
-  );
-
   return (
     <div
       className="App"
       style={{ backgroundImage: `url(${require('./assets/img/kosmos-big.jpg')})` }}
     >
-      {/* <MainLayout>
+      <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/messages" element={<Messages />} />
@@ -46,10 +21,7 @@ function App() {
           <Route path="/b/:id" element={<Boards />} />
           <Route path="/c/:id" element={<Boards />} />
         </Routes>
-      </MainLayout> */}
-      <Routes>
-        <Route path="/test" element={<Test />} />
-      </Routes>
+      </MainLayout>
     </div>
   );
 }
